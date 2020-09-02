ad341faf69d2d66a4ef8599c0b2483dde1e30410
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2020-09-02 11:26:57 -0400

    Add files via upload

---
+++ index-script.js
@@ -0,0 +1,7 @@
+
+function myfunc()
+{	
+	console.log(document.getElementById('name').value);	
+	console.log(document.getElementById('date').value);	
+	console.log(document.getElementById('comment').value);	
+}
